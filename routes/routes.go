b7d1089b2ebb73aa97b59ccc265eb98d91866388
Akin-Kilic <akinkilic24@gmail.com>
2023-11-04 17:44:26 +0300

    modified routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Akin-Kilic/ecommerce/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func EndPoints(app *fiber.App) {
-	app.Post("/users/signup", SignUp)
-	app.Post("/users/login", Login)
-	app.Post("/admin/addprpducts", ProductViewerAdmin)
-	app.Get("/users/productview", SearchProduct)
-	app.Get("/users/search", SearchProductByQuery)
+	app.Post("/users/signup", controllers.SignUp())
+	app.Post("/users/login", controllers.Login())
+	app.Post("/admin/addprpducts", controllers.ProductViewerAdmin())
+	app.Get("/users/productview", controllers.SearchProduct())
+	app.Get("/users/search", controllers.SearchProductByQuery())
 }
