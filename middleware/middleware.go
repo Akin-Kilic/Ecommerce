93de7b03a98c0dfe104daf34969858af60d0f515
Akin-Kilic <akinkilic24@gmail.com>
2023-11-05 19:42:24 +0300

    created middleware.go

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,22 @@
 package middleware
+
+import (
+	"net/http"
+
+	token "github.com/Akin-Kilic/ecommerce/tokens"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Authentication(c *fiber.Ctx) error {
+	ClientToken := c.Get("token")
+	if ClientToken == "" {
+		return c.Status(http.StatusInternalServerError).JSON("No Authorization Header Provided")
+	}
+	claims, err := token.ValidateToken(ClientToken)
+	if err != "" {
+		return c.Status(fiber.StatusInternalServerError).JSON(err)
+	}
+	c.Set("email", claims.Email)
+	c.Set("uid", claims.Uid)
+	return c.Next()
+}
