fbfa0bc5b4c5f38614c8dfdab9a2dac30aa18c13
Akin-Kilic <akinkilic24@gmail.com>
2023-11-06 00:38:10 +0300

    modied mongo db connection

--- database/database.go
+++ database/database.go
@@ -8,11 +8,9 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func DBSet() *mongo.Client {
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
@@ -20,20 +18,11 @@ func DBSet() *mongo.Client {
 		log.Fatal(err)
 	}
 
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
-
-	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-	err = client.Ping(ctx, readpref.Primary())
+	err = client.Ping(context.TODO(), nil)
 	if err != nil {
 		log.Println("failed to connect to mongodb")
 		return nil
 	}
-
 	fmt.Println("Successfully Connected to the mongodb")
 	return client
 }
@@ -43,6 +32,7 @@ var Client *mongo.Client = DBSet()
 func UserData(client *mongo.Client, CollectionName string) *mongo.Collection {
 	var collection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
 	return collection
+
 }
 
 func ProductData(client *mongo.Client, CollectionName string) *mongo.Collection {
